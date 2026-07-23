031494fc3c847330e26a8da7c78c2db550fcf6aa
Md. Biplob Talukdar <biplob.codes@gmail.com>
2026-07-24 02:13:23 +0600

    Update the schema for team members table

--- db/migrations/000007_create_team_members.down.sql
+++ db/migrations/000007_create_team_members.down.sql
@@ -1,4 +1,30 @@
-ALTER TABLE characters ADD COLUMN team_id INTEGER REFERENCES teams(id) ON DELETE SET NULL;
-ALTER TABLE teams ADD COLUMN sensei_id INTEGER REFERENCES characters(id) ON DELETE RESTRICT;
+PRAGMA foreign_keys=off;
+
+DROP TABLE characters;
+CREATE TABLE characters (
+    id         INTEGER PRIMARY KEY,
+    name       TEXT NOT NULL,
+    nickname   TEXT,
+    clan       TEXT,
+    age        INTEGER,
+    rank       TEXT NOT NULL CHECK (rank IN ('Genin','Chunin','Jonin','Kage')),
+    birthdate  DATE NOT NULL,
+    village_id INTEGER NOT NULL,
+    team_id    INTEGER REFERENCES teams(id) ON DELETE SET NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (village_id) REFERENCES villages(id) ON DELETE RESTRICT
+);
+
+DROP TABLE teams;
+CREATE TABLE teams (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    sensei_id INTEGER NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sensei_id) REFERENCES characters(id) ON DELETE RESTRICT
+);
 
 DROP TABLE team_members;
+
+PRAGMA foreign_keys=on;
--- db/migrations/000007_create_team_members.up.sql
+++ db/migrations/000007_create_team_members.up.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys=off;
+
 CREATE TABLE team_members (
     team_id INTEGER NOT NULL,
     character_id INTEGER NOT NULL,
@@ -7,5 +9,27 @@ CREATE TABLE team_members (
     FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE,
     FOREIGN KEY (character_id) REFERENCES characters(id) ON DELETE CASCADE
 );
-ALTER TABLE teams DROP COLUMN sensei_id;
-ALTER TABLE characters DROP COLUMN  team_id;
+
+DROP TABLE teams;
+CREATE TABLE teams (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE characters;
+CREATE TABLE characters (
+    id         INTEGER PRIMARY KEY,
+    name       TEXT NOT NULL,
+    nickname   TEXT,
+    clan       TEXT,
+    age        INTEGER,
+    rank       TEXT NOT NULL CHECK (rank IN ('Genin','Chunin','Jonin','Kage')),
+    birthdate  DATE NOT NULL,
+    village_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (village_id) REFERENCES villages(id) ON DELETE RESTRICT
+);
+
+PRAGMA foreign_keys=on;
